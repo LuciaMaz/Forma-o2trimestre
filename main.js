c55d1ce498f34dcd3aa341dc94d977ae5d068c99
LuciaMaz <124603806+LuciaMaz@users.noreply.github.com>
2025-06-12 14:38:46 -0300

    ok

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-alert('olá');
+let nomeUsuario="";
+let elemento=document.querySelector("#nome_usuario");
+while(nomeUsuario==""){
+    nomeUsuario=prompt("Qual o seu nome?");
+}
+if(nomeUsuario==null){
+    elemento.textContent='seja muito bem-vindo';
+}else{
+    elemento.textContent=nomeUsuario;
+}
